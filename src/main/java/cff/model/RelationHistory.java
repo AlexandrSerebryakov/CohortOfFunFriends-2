54913585725c80a0bfe503b539c94530fe555a99
AlexandrSerebryakov <common190516@mail.ru>
2019-01-21 20:56:24 +0600

    Create boolean variables in "RelationsHistory" class.

--- src/main/java/cff/model/RelationHistory.java
+++ src/main/java/cff/model/RelationHistory.java
@@ -4,4 +4,6 @@ package cff.model;
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
 public class RelationHistory {
-}
+    boolean isIni, isReg, isMet, isCom, isNk, s, isSphoto, isSvideo, isIphoto, isIvideo,
+            isStop, isNotHi, isTerminate, isBreakHi, isReRelations, isReIni, isLove, isReLove;
+    }
