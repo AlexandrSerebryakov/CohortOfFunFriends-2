756d22900b48cb1de61a226be4222d3a02e8890d
AlexandrSerebryakov <common190516@mail.ru>
2019-01-23 21:32:24 +0600

    Build class "Person"

--- src/main/java/cff/model/Person.java
+++ src/main/java/cff/model/Person.java
@@ -6,6 +6,8 @@ import java.util.Date;
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
 public class Person {
+    private int id;
+    //TODO base entity as ID
     private String name;
     private String surname;
     private String middleName;
@@ -17,6 +19,19 @@ public class Person {
 
     //FIXME Fields: name, surname, middleName become not access or
     //FIXME even invisible
+    private boolean isCharisma;
+    private boolean isShock;
+    private boolean isNb;
+    private boolean isBb;
+    private String bpl;
+    private enum education {INCOMPLETE_SECONDARY, SECONDARY, COLLEGE, BACHELOR, MASTER, PHD};
+    private String placeOfEducation1;
+    private String placeOfEducation2;
+    private String placeOfEducation3;
+    private String placeOfJob1;
+    private String placeOfJob2;
+    private String placeOfJob3;
+    private String comment;
 
 
 }
