c71b76604fe55e703ec67623055006c3f0be092e
AlexandrSerebryakov <common190516@mail.ru>
2019-01-23 21:00:58 +0600

    Build class "Person"

--- src/main/java/cff/model/Person.java
+++ src/main/java/cff/model/Person.java
@@ -1,7 +1,22 @@
 package cff.model;
 
+import java.util.Date;
+
 /**
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
 public class Person {
+    private String name;
+    private String surname;
+    private String middleName;
+    private byte age;
+    //TODO Realise automatic counter of age on today
+    private Date dob;
+    private boolean isAnonymous;
+    //TODO Realise functional: if anonymous is true then
+
+    //FIXME Fields: name, surname, middleName become not access or
+    //FIXME even invisible
+
+
 }
--- src/main/java/cff/model/RelationshipHistory.java
+++ src/main/java/cff/model/RelationshipHistory.java
@@ -3,7 +3,7 @@ package cff.model;
 /**
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
-public class RelationHistory {
-    boolean isIni, isReg, isMet, isCom, isNk, isS, isSphoto, isSvideo, isIphoto, isIvideo,
+public class RelationshipHistory {
+    private boolean isIni, isReg, isMet, isCom, isNk, isS, isSphoto, isSvideo, isIphoto, isIvideo,
             isStop, isNotHi, isTerminate, isBreakHi, isReRelations, isReIni, isLove, isReLove;
     }
