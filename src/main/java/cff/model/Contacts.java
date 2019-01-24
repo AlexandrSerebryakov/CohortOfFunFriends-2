4ea82df93e2fc68f0e7ab893b515c6b05d98c295
AlexandrSerebryakov <common190516@mail.ru>
2019-01-24 21:17:37 +0600

    Create model: class Contacts

--- src/main/java/cff/model/Contacts.java
+++ src/main/java/cff/model/Contacts.java
@@ -4,4 +4,24 @@ package cff.model;
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
 public class Contacts {
-}
+
+    private int id;
+    private String residentialAddress1;
+    private String residentialAddress2;
+    private String residentialAddress3;
+    //TODO Think about magic numbers
+    private String legalAddress1;
+    private String legalAddress2;
+    private String legalAddress3;
+    //TODO Think about magic numbers
+    private int telephoneNumber1;
+    private int telephoneNumber2;
+    private int telephoneNumber3;
+    private int telephoneNumber4;
+    private int telephoneNumber5;
+    private int telephoneNumber6;
+    //TODO Think about magic numbers
+    private String comment;
+
+
+    }
