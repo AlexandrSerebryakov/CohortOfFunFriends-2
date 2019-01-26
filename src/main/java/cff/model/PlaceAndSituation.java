1a4b35df7ed4808d2f58b7a05b863c760329eca0
AlexandrSerebryakov <common190516@mail.ru>
2019-01-26 23:55:57 +0600

    Create model: class "PlaceAndSituation", module "Conductor"

--- src/main/java/cff/model/PlaceAndSituation.java
+++ src/main/java/cff/model/PlaceAndSituation.java
@@ -4,4 +4,13 @@ package cff.model;
  * Created by ${AlexandrSerebryakov} on ${09.10.2016}.
  */
 public class PlaceAndSituation {
+
+    private String placeOfAcquaintance;
+    private String circumstancesOfDating;
+    private boolean conductor;
+    private byte busNumber;
+    private String stateNumber;
+    private String from;
+    private String to;
+
 }
