63bbc9e02efc57d30daec473bb59bbe3260412d9
AlexandrSerebryakov <common190516@mail.ru>
2019-02-26 19:03:25 +0600

    Canceled from joda-time and joda-money, because use jdk-8

--- src/main/java/cff/main/Runner.java
+++ src/main/java/cff/main/Runner.java
@@ -7,5 +7,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Good luck!!!");
         // TODO: 27.01.2019 Think about enum-classes vs enum-fields
+        System.out.println(System.getProperties());
     }
 }
